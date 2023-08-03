6703b747e70ca6d78923df1bb008fa21466fd184
public202002 <77833368+public202002@users.noreply.github.com>
2023-08-03 19:39:43 +0200

    Create HowTo.txt.js

---
+++ HowTo.txt.js
@@ -0,0 +1,7 @@
+// Docker Integration:      https://codesandbox.io/docs/learn/environment/docker
+// CodeSandbox Repositories https://codesandbox.io/docs/learn/repositories/overview
+
+// Codesandbox hint:    Ctrl + K
+
+// https://codesandbox.io/p/github/public202002/GoogleLogIn_react/main?create=true&file=/src/App.js:1,1
+// https://codesandbox.io/s/google-login-forked-ck7dnl
